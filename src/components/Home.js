eeacdbae035d5c10c44ce8da1a89a9a9bf05f67c
semz-ui <michaeltimilehin36@gmail.com>
2022-05-09 20:08:29 -0500

    new updat

--- src/components/Home.js
+++ src/components/Home.js
@@ -54,6 +54,8 @@ function Home() {
           onSubmit={(e) => {
             submitForm(e);
           }}
+          method='POST'
+          dete-netlify='true'
         >
           <div>
             <label>
